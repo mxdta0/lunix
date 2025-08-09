91029fde1f57cc89e9937f4666f5d3fb35c0ce2d
mxdta0 <169173305+mxdta0@users.noreply.github.com>
2025-08-09 13:20:59 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "lunix-bot",
+  "version": "1.0.0",
+  "main": "start.js",
+  "scripts": {
+    "start": "node src/bot.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.4.0",
+    "node-fetch": "^2.6.7",
+    "unzipper": "^0.10.11"
+  }
+}
